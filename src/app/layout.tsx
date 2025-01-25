358cd0b655a685a5ea1dd8d85bb87082ca60c976
Öykü Kaya <oykuky001@gmail.com>
2025-01-26 00:07:12 +0300

    feat: add layout structure and Tailwind configuration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
+import { LeftBar } from "@/components/layout/LeftBar";
 import "./globals.css";
+import { RightBar } from "@/components/layout/RightBar";
 
 export default function RootLayout({
   children,
@@ -7,7 +9,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <div className="max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl xxl:max-w-screen-xxl flex mx-auto justify-between ">
+          <div className="px-2 xs:px-4 xxl:px-8 h-screen">
+            <LeftBar />
+          </div>
+          <div className="flex-1 lg:max-w-[600px] border-x-[1px] border-borderGray">
+            {children}
+          </div>
+          <div className="hidden lg:flex ml-4 md:ml-8 flex-1">
+            <RightBar />
+          </div>
+        </div>
+      </body>
     </html>
   );
 }
