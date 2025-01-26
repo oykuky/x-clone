f58e83c5faacc2920c135ebb9c0b8c3ed38b158e
Öykü Kaya <oykuky001@gmail.com>
2025-01-27 00:31:24 +0300

    feat: add SVG support and create initial feed components

---
+++ public/icons/index.ts
@@ -0,0 +1,23 @@
+export {default as BackIcon} from './back.svg';
+export {default as BookmarkIcon} from './bookmark.svg';
+export {default as CommunityIcon} from './community.svg';
+export {default as DateIcon} from './date.svg';
+export {default as EmojiIcon} from './emoji.svg';
+export {default as ExploreIcon} from './explore.svg';
+export {default as GifIcon} from './gif.svg';
+export {default as HomeIcon} from './home.svg';
+export {default as ImageIcon} from './image.svg';
+export {default as JobIcon} from './job.svg';
+export {default as InfoMoreIcon} from './infoMore.svg';
+export {default as LocationIcon} from './location.svg';
+export {default as LogoIcon} from './logo.svg';
+export {default as MessageIcon} from './message.svg';
+export {default as MoreIcon} from './more.svg';
+export {default as PollIcon} from './poll.svg';
+export {default as PostIcon} from './post.svg';
+export {default as ProfileIcon} from './profile.svg';
+export {default as ScheduleIcon} from './schedule.svg';
+export {default as UserLocationIcon} from './userLocation.svg';
+export {default as NotificationIcon} from './notification.svg';
+
+
---
+++ src/components/feed/Feed.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { PostCard } from './PostCard'
+
+export const Feed = () => {
+  return (
+    <div>
+        <PostCard />
+    </div>
+  )
+}
