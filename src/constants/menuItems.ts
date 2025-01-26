4eb278e86c362d31befa6a39515fcc30342773a0
Öykü Kaya <oykuky001@gmail.com>
2025-01-26 20:07:38 +0300

    feat: integrate ImageKit for optimized image loading and add layout components

---
+++ src/constants/menuItems.ts
@@ -0,0 +1,62 @@
+export const menuList = [
+    {
+      id: 1,
+      name: "Homepage",
+      link: "/",
+      icon: "home.svg",
+    },
+    {
+      id: 2,
+      name: "Explore",
+      link: "/",
+      icon: "explore.svg",
+    },
+    {
+      id: 3,
+      name: "Notification",
+      link: "/",
+      icon: "notification.svg",
+    },
+    {
+      id: 4,
+      name: "Messages",
+      link: "/",
+      icon: "message.svg",
+    },
+    {
+      id: 5,
+      name: "Bookmarks",
+      link: "/",
+      icon: "bookmark.svg",
+    },
+    {
+      id: 6,
+      name: "Jobs",
+      link: "/",
+      icon: "job.svg",
+    },
+    {
+      id: 7,
+      name: "Communities",
+      link: "/",
+      icon: "community.svg",
+    },
+    {
+      id: 8,
+      name: "Premium",
+      link: "/",
+      icon: "logo.svg",
+    },
+    {
+      id: 9,
+      name: "Profile",
+      link: "/",
+      icon: "profile.svg",
+    },
+    {
+      id: 10,
+      name: "More",
+      link: "/",
+      icon: "more.svg",
+    },
+  ];
